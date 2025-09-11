567911c686d5aa2fa5aafc9d54db7d54bcd47dfa
Lucas Gabriel da Silva Lima <lucas.silva.lima11@escola.pr.gov.br>
2025-09-11 10:48:38 -0300

    .

--- sketch.js
+++ sketch.js
@@ -2,61 +2,56 @@ function setup() {
   createCanvas(400, 400);
 }
 
-let xJogador1 = 0;
-let xJogador2 = 0;
+let xJogador = [0, 0, 0, 0];
+let yJogador = [75, 150, 225, 300];
+let jogador = ["😎", "❤️", "👽", "🚘"];
+let teclas = ["a", "s", "d", "f"];
+let quantidade = jogador.length;
 
 function draw() {
-  ativaJogo()
-  desenhaJogadores()
-  desenhaLinhaDeChegada()
-  verificarVencedor()
+  ativaJogo();
+  desenhaJogadores();
+  desenhaLinhaDeChegada();
+  verificaVencedor();
 }
-  function ativaJogo(){
-    if(focused == true){
-       background(220);
-    } else {
-       background('rgb(11,129,109)');
-    }
-  }
-function desenhaJogadores(){
-   textSize(40);
-  text("🥵", xJogador1, 100);
-  text("🤬", xJogador2, 200);
-}
-  function desenhaLinhaDeChegada(){
-    fill('white')
-    rect(350, 0, 10, 400);
-    fill('black')
-    for (let yAtual = 0; yAtual<400; yAtual +=20) {  rect(350, yAtual, 10, 10);}
-  }
-    function verificarVencedor() {
-      if (xJogador1 > 350) {
-    text("Jogador 1 venceu!", 30, 200);
-    noLoop(); }
-    }
-    if (xJogador2 > 350){
-    text("Jogador 2 venceu!", 30, 200);
-    noLoop(); 
-    }
 
-funtion keyReleased() {
-  if (key =="a"){
-      xJogador1 += random(20);
-  }
-  if (key =="s"){
-    xJogador2 += random(5);
+function ativaJogo() {
+  if (focused == true) {
+    background("#D2EBB5");
+  } else {
+    background("rgb(238,178,178)");
   }
 }
 
-  
-  xJogador1 = xJogador1 + random(5);
-  xJogador2 = xJogador2 + random(5);
-  if (xJogador1 > 350) {
-    text("Jogador 1 venceu!", 30, 200);
-    noLoop();
+function desenhaJogadores() {
+  textSize(40);
+  for (let i = 0; i < quantidade; i++) {
+    text(jogador[i], xJogador[i], yJogador[i]);
   }
-  if (xJogador2 > 350) {
-    text("Jogador 2 venceu!", 30, 200);
-    noLoop();
-  
+}
+
+function desenhaLinhaDeChegada() {
+  fill("white");
+  rect(350, 0, 10, 400);
+  fill("black");
+  for (let yAtual = 0; yAtual < 400; yAtual += 20) {
+    rect(350, yAtual, 10, 10);
+  }
+}
+
+function verificaVencedor() {
+  for (let i = 0; i < quantidade; i++) {
+    if (xJogador[i] > 350) {
+      text(jogador[i] + " venceu!", 50, 200);
+      noLoop();
+    }
+  }
+}
+
+function keyReleased() {
+  for (let i = 0; i < quantidade; i++) {
+    if (key == teclas[i]) {
+      xJogador[i] += random(20);
+    }
+  }
 }
